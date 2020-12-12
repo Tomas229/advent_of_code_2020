b83aa808d2b38f439dd39a78c88546dc9820f2af
Tomas229 <53621992+Tomas229@users.noreply.github.com>
2020-12-11 22:55:32 -0300

    day 11 done

--- day11/day11.py
+++ day11/day11.py
@@ -67,5 +67,115 @@ print(equilibrium([list("L.LL.LL.LL"),
                    list("L.LLLLLL.L"),
                    list("L.LLLLL.LL")]))
 
-print(equilibrium(input_file))
+# print(equilibrium(input_file))
 # 1:30
+
+
+def get_adjacent_indices2(i, j, l):
+    adjacent_indices = []
+    len_x = len(l)
+    len_y = len(l[0])
+    x = i
+    y = j
+    while(x + 1 < len_x):
+        x += 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while x > 0:
+        x -= 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while(y + 1 < len_y):
+        y += 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while y > 0:
+        y -= 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while(x + 1 < len_x and y + 1 < len_y):
+        x += 1
+        y += 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while(x + 1 < len_x and y > 0):
+        x += 1
+        y -= 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while(x > 0 and y > 0):
+        x -= 1
+        y -= 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+    x = i
+    y = j
+    while(x > 0 and y + 1 < len_y):
+        x -= 1
+        y += 1
+        if(l[x][y] != "."):
+            adjacent_indices.append((x, y))
+            break
+
+    return adjacent_indices
+
+
+def equilibrium2(l):
+    x = len(l)
+    y = len(l[0])
+    new = copy.deepcopy(l)
+    while True:
+        l = copy.deepcopy(new)
+        for i in range(x):
+            for j in range(y):
+                adj = get_adjacent_indices2(i, j, l)
+                hash_count = 0
+                for a in adj:
+                    if l[a[0]][a[1]] == "#":
+                        hash_count += 1
+                if l[i][j] == "L" and hash_count == 0:
+                    new[i][j] = "#"
+                if l[i][j] == "#" and hash_count > 4:
+                    new[i][j] = "L"
+        if(new == l):
+            break
+    count = 0
+    for i in range(x):
+        for j in range(y):
+            if l[i][j] == "#":
+                count += 1
+    return count
+
+
+print(equilibrium2([list("L.LL.LL.LL"),
+                    list("LLLLLLL.LL"),
+                    list("L.L.L..L.."),
+                    list("LLLL.LL.LL"),
+                    list("L.LL.LL.LL"),
+                    list("L.LLLLL.LL"),
+                    list("..L.L....."),
+                    list("LLLLLLLLLL"),
+                    list("L.LLLLLL.L"),
+                    list("L.LLLLL.LL")]))
+print(equilibrium2(input_file))
+
+#40 minsz
